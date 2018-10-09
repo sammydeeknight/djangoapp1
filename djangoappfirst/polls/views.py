6e7ea0965927b96a3d15b1be9377f6261a716d1b
sammydeeknight <sammydeeknight@gmx.com>
2018-10-09 00:39:42 -0700

    i've solved the indentation problem

--- djangoappfirst/polls/views.py
+++ djangoappfirst/polls/views.py
@@ -6,13 +6,20 @@ from . import views
 
 from .models import Choice, Question
 
-class IndexView(generic.ListView): template_name = 'polls/index.html' context_object_name = 'latest_question_list'
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def get_queryset(self): return Question.objects.order_by('-pub_date')[:5]
+def get_queryset(self):
+    return Question.objects.order_by('-pub_date')[:5]
 
-class DetailView(generic.DetailView): model = Question template_name = 'polls/detail.html'
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-class ResultView(generic.DetailView): model = Question template_name = 'polls/results.html'
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def question(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
